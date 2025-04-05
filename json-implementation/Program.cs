2df57ed521bbda24d6673418af90753ef4d6b298
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-04-05 16:19:41 +0700

    adding main class. in main branch

--- json-implementation/Program.cs
+++ json-implementation/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Main Program
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
