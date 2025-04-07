e7e8a64926e12c7eea2fdf496cf974ac56f77424
Muhammad Arzu <68320293+Arzu95@users.noreply.github.com>
2025-04-07 22:38:36 +0700

    Menambahkan 3 class
    bang izin upload file langsung dari github, soalnya dari visual studio nya gak bisa committ aneh banget

---
+++ json-implementation/DataMahasiswa103022300097.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace json_implementation
+{
+    class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+    class DataMahasiswa103022300097
+    {
+        public Nama nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; }
+
+        public static void ReadJSON()
+        {
+            string filePath = @"C:\Users\hp\Documents\Project KPL\MOD7\json-implementation\tp7_1_103022300097";
+
+            // Baca isi file JSON
+            string jsonString = File.ReadAllText(filePath);
+
+            // Deserialize ke object
+            DataMahasiswa103022300097 data = JsonSerializer.Deserialize<DataMahasiswa103022300097>(jsonString);
+
+            // Tampilkan hasil deserialisasi
+            Console.WriteLine($"Nama: {data.nama.depan} {data.nama.belakang}");
+            Console.WriteLine($"NIM: {data.nim}");
+            Console.WriteLine($"Fakultas: {data.fakultas}");
+        }
+    }
+}
---
+++ json-implementation/KuliahMahasiswa103022300097.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace json_implementation
+{
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class CourseList
+    {
+        public List<Course> courses { get; set; }
+    }
+
+    class KuliahMahasiswa103022300097
+    {
+
+        public static void ReadJSON()
+        {
+            try
+            {
+
+                string filePath = @"C:\Users\hp\Documents\Project KPL\MOD7\json-implementation\tp7_2_103022300097";
+
+                // Baca isi file JSON
+                string jsonString = File.ReadAllText(filePath);
+
+                // Deserialize ke object
+                var data = JsonSerializer.Deserialize<CourseList>(jsonString);
+
+                foreach (var course in data.courses)
+                {
+                    Console.WriteLine($"Kode: {course.code}, Nama: {course.name}");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
